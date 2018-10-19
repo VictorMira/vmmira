d4c6b1e02c01c27251f943c57b3dd563f6a0dc33
Victor Mira <coding.reakktor@gmail.com>
2018-10-18 21:43:37 -0300

    testing css

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import './header.scss'
+
 const Header = ({ siteTitle }) => (
-  <div>
-    <div>
+    <header className='header'>
       <span className='logo'>VM</span>
-      <h1 style={{ margin: 0 }}>
+      <h1 className='siteTitle'>
         <Link to="/">
           {siteTitle}
         </Link>
       </h1>
-
-      <header className='header'>
-        <div className='header-wrap'>
-          <nav className='main-nav'>
-            <ul>
-              <li><Link className='about' to='/about'>About</Link></li>
-              <li><Link className='projects' to='/projects'>Projects</Link></li>
-              <li><Link className='blog' to='/blog'>Blog</Link></li>
-              <li><Link className='contact' to='/contact'>Contact</Link></li>
-            </ul>
-          </nav>
-        </div>
-
-      </header>
-    </div>
-  </div>
+      <nav className='main-nav'>
+        <ul>
+          <li className='active'><Link className='about' to='/about'>About</Link></li>
+          <li><Link className='projects' to='/projects'>Projects</Link></li>
+          <li><Link className='blog' to='/blog'>Blog</Link></li>
+          <li><Link className='contact' to='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
+    </header>
 )
 
 export default Header
